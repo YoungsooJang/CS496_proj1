7813d027175035f7790646cc59ff7bf65682f237
Anders <anders.wk.choi@gmail.com>
2016-12-27 22:01:18 +0900

    added text/call feature

--- app/src/main/java/com/example/q/cs496_proj1/ContactDisplayActivity.java
+++ app/src/main/java/com/example/q/cs496_proj1/ContactDisplayActivity.java
@@ -1,9 +1,12 @@
 package com.example.q.cs496_proj1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +28,8 @@ public class ContactDisplayActivity extends AppCompatActivity {
         TextView textView1 = (TextView)findViewById(R.id.textView1);
         TextView textView2 = (TextView)findViewById(R.id.textView2);
         ImageView imageView1 = (ImageView)findViewById(R.id.imageView1);
+        Button button1 = (Button)findViewById(R.id.button1);
+        Button button2 = (Button)findViewById(R.id.button2);
 
         textView1.setText(name);
         textView2.setText(number);
@@ -32,5 +37,20 @@ public class ContactDisplayActivity extends AppCompatActivity {
             imageView1.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView1.setImageResource(picture);
         }
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stripped = number.replace("-", "");
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + stripped)));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stripped = number.replace("-", "");
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + stripped)));
+            }
+        });
     }
 }
